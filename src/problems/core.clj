5d8310e1982726b10f9f075f95ee70bec976f136
Manoj Mathai <manojpmathai@gmail.com>
2012-03-08 13:41:55 +0530

    Replaced some ugly inner loops with reduce, added a version that swaps chars to generate permutations.

--- src/problems/core.clj
+++ src/problems/core.clj
@@ -7,7 +7,10 @@
   (let [s "abcdefghi"
         l (.length s)
         expected-perms (reduce * (range 1 (inc l))) ]
-    (time  (println (count  (permutations-by-inject-recursive s))))
-    (time  (println (count  (permutations-by-inject-recursive-faster s))))
-    (time  (println (count  (permutations-by-inject-iterative s))))
-    (time  (println (count  (permutations-by-inject-iterative-faster s))))))
+    (dotimes [n 10]
+      (println)
+      (println (str  "#" n))
+      (-> s permutations-by-inject-recursive count println time)
+      (-> s permutations-by-inject-iterative count println time)
+      (-> s permutations-by-swap-iterative count println time)
+      )))
--- src/problems/permutations.clj
+++ src/problems/permutations.clj
@@ -1,15 +1,15 @@
 (ns problems.core
   (:require [clojure.string :as cs]))
 
-(defn break-str-at [^String s, ^long n]
+(defn- break-str-at [^String s, ^long n]
   [(.substring s 0 n)
    (.substring s n)])
 
-(defn all-str-pieces [^String s]
+(defn- all-str-pieces [^String s]
   (map #(break-str-at s %)
        (range (inc (.length s)))))
 
-(defn inject-at-all-pos [^String s, ^String c]
+(defn- inject-at-all-pos [^String s, ^String c]
   (let [pieces (all-str-pieces s)]
     (map #(str (% 0) c (% 1))
          pieces)))
@@ -20,23 +20,9 @@
       [s, (cs/reverse s)]
       (let [[head, tail] (break-str-at s 1)
             perms (permutations-by-inject-recursive tail)]
-        (flatten (map #(inject-at-all-pos % head)
-                      perms))))))
-
-(defn permutations-by-inject-recursive-faster [^String s]
-  (let [len (.length s)]
-    (if (= 2 len)
-      [s, (cs/reverse s)]
-      (let [[head, tail] (break-str-at s 1)
-            tail-perms (permutations-by-inject-recursive-faster tail)]
-        (loop [tperms tail-perms
-               all-perms []]
-          (let [p (first tperms)]
-            (if p
-              (recur (next tperms)
-                     (into all-perms
-                           (inject-at-all-pos p head)))
-              all-perms)))))))
+        (reduce #(into %1 %2) []
+                (map #(inject-at-all-pos % head)
+                     perms))))))
 
 (defn permutations-by-inject-iterative [^String s]
   (let [[head, tail] (break-str-at s 2)]
@@ -44,22 +30,31 @@
            rem tail]
       (let [char (first rem)]
         (if char
-          (recur (vec (flatten (map #(inject-at-all-pos % char) perms)))
+          (recur (reduce #(into %1 %2) []
+                         (map #(inject-at-all-pos % char) perms))
                  (next rem))
           perms)))))
 
-(defn permutations-by-inject-iterative-faster [^String s]
-  (let [[head, tail] (break-str-at s 2)]
-    (loop [perms [head, (cs/reverse head)]
-           rem tail]
-      (let [char (first rem)]
-        (if char
-          (let [new-perms (loop [acc []
-                                 p perms]
-                            (let [fp (first p)]
-                              (if fp
-                                (recur (into acc (inject-at-all-pos fp char)) (next p))
-                                acc)))]
-            (recur new-perms (next rem)))
-          perms)))))
+(defn- str-swap-chars-at [^String s, ^long i, ^long j]
+  (let [chars (vec s)
+        char1 (chars i)
+        char2 (chars j)
+        swapped (assoc chars
+                  i char2
+                  j char1)]
+    (apply str swapped)))
 
+(defn- swaps [^String s, ^long i]
+  (map #(str-swap-chars-at s i %)
+       (range (inc i) (.length s))))
+
+(defn permutations-by-swap-iterative [^String s]
+  (let [n (dec (.length s))]
+       (loop [perms [s]
+              i 0]
+         (if (> i n)
+           perms
+           (recur (into perms
+                        (reduce #(into %1 %2) []
+                                (map #(swaps % i) perms)))
+                  (inc i))))))
